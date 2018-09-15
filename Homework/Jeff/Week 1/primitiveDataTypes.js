eb196419d5e3d01a454230547e6b2a88bbf7c3d2
Raymond427 <rferrel427@gmail.com>
2018-09-15 17:16:58 -0400

    Week 1 and 2 Homework

---
+++ Homework/Jeff/Week 1/primitiveDataTypes.js
@@ -0,0 +1,45 @@
+// What are the only two things computers understand?
+
+// How do we turn our code into something the computer can understand?
+
+// What is the concept of removing complex details that are not necessary to simplify something?
+
+// What language/s could you use to make a video game physics engine that has to solve thousands of complex math problems sixty times every second?
+
+// What language/s could you use to animate something on a website?
+
+// What language/s could you use to make software that must run on many machines, some you may never even know are using your app?
+
+// What are the three ways to declare a string in Javascript? Create three strings below in three different ways
+// i.e. 'Hi, I'm Raymond'
+
+// Multiple Choice! Delete the answer that is INCORRECT
+// Primitive Data Types are mutable, meaning they can be changed during runtime, (i.e. while the program is running)
+// True     False
+
+// Primitive Data Types are objects and have no methods
+// True     False
+
+// Although null is a Primitive Data Type, if you check it's type Javascript will say it's an object
+// True     False
+
+// Negative Numbers are truthy in JavaScript
+// True     False
+
+// When a math problem like dividing by zero, is unsolvable, what does Javascript return?
+
+// When should you use the Primitive Data Type undefined when writting code?
+
+// Whats the only Primitive Data Type that doesn't equal itself?
+
+// What are the reserved keywords you can use to make new data? Make three pieces of data below with each declaration keyword?
+
+// Which declaration keywords makes mutable data? (hunt there are two)
+
+// Which keyword makes immutable data?
+
+// Which keyword prevents hoisting
+
+// When should you use if, if else and else
+
+// When Should you use a while loop instead of a for loop?
